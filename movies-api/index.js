a8e92e43bc75cab1bb946a7ed5c098347e7891ad
Liam Crowe <20093470@mail.wit.ie>
2023-12-20 01:15:34 +0000

    Added Auth Middleware

--- movies-api/index.js
+++ movies-api/index.js
@@ -3,6 +3,8 @@ import dotenv from 'dotenv';
 import cors from 'cors';
 import usersRouter from './api/users';
 import moviesRouter from './api/movies';
+import authenticate from './authenticate';
+
 import './db';
 import defaultErrHandler from './errHandler'
 
@@ -15,6 +17,7 @@ app.use(cors());
 app.use(express.json());
 app.use('/api/movies', moviesRouter); 
 app.use('/api/users', usersRouter);
+app.use('/api/movies',authenticate,  moviesRouter);
 app.use(defaultErrHandler);
 
 app.listen(port, () => {
