9159e279a44146e7f2b0da30749e6a260e678abd
Liam Crowe <20093470@mail.wit.ie>
2023-12-20 00:36:36 +0000

    Initialise Dev Env - Data

--- tasky-api/index.js
+++ tasky-api/index.js
@@ -1,8 +1,10 @@
+import cors from 'cors';
 import './db';
 import dotenv from 'dotenv';
 import express from 'express';
 import tasksRouter from './api/tasks';
 import usersRouter from './api/users';
+
 
 dotenv.config();
 
@@ -18,6 +20,7 @@ const errHandler = (err, req, res, next) => {
 const app = express();
 const port = process.env.PORT;
 
+app.use(cors());
 app.use(express.json());
 app.use('/api/tasks', tasksRouter);
 app.use(errHandler);
