9b004e1d817738614e2f7eb1e7930336eda711c3
Liam Crowe <20093470@mail.wit.ie>
2023-12-22 16:04:06 +0000

    Attempting to add password change

--- reactApp/src/api/movies-api.js
+++ reactApp/src/api/movies-api.js
@@ -16,15 +16,6 @@ export const getUpcomingMovies = async () => {
   return response.json();
 };
 
-export const getTrendingMovies = async () => {
-  const response = await fetch('http://localhost:8080/api/movies/tmdb/trending', {
-    headers: {
-      'Authorization': window.localStorage.getItem('token')
-    }
-  });
-  return response.json();
-};
-
 export const login = async (username, password) => {
   const response = await fetch('http://localhost:8080/api/users', {
     headers: {
